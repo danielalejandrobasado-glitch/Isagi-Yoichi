ff72618b1d23b88522bf814b758ae0982f8d56f1
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2025-08-06 15:22:44 -0500

    💙HATSUNE MIKU💙

--- plugins/anime-blush.js
+++ plugins/anime-blush.js
@@ -27,7 +27,7 @@ let handler = async (m, { conn, usedPrefix }) => {
         let pp8 = 'https://files.catbox.moe/po2fxx.mp4'
 
         
-        const videos = [pp, pp2, pp3, pp4, pp5, pp6, pp7, pp8, pp9, pp10, pp11, pp12, pp13, pp14, pp15]
+        const videos = [pp, pp2, pp3, pp4, pp5, pp6, pp7, pp8]
         const video = videos[Math.floor(Math.random() * videos.length)]
         
         conn.sendMessage(m.chat, { video: { url: video }, gifPlayback: true, caption: str, ptt: true, mentions: [who] }, { quoted: m })
