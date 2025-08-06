063a4bb702cb2af3c0a93b9548cc7b2aba19a6d8
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2025-08-06 14:04:41 -0500

    💙HATSUNE MIKU💙

--- plugins/ai-dalle.js
+++ plugins/ai-dalle.js
@@ -1,10 +1,8 @@
-// 🎤💙 Código creado por Brauliovh3 >> https://github.com/Brauliovh3/HATSUNE-MIKU
-
 import axios from 'axios';
 
 const handler = async (m, { conn, args }) => {
     if (!args[0]) {
-        await conn.reply(m.chat, `🎤💙 Por favor, proporciona una descripción para generar la imagen en el mundo virtual de Miku ✨🎵`, m);
+        await conn.reply(m.chat, `💙 Por favor, proporciona una descripción para generar la imagen en el mundo virtual de Miku 🎵`, m, rcanal);
         return;
     }
 
@@ -12,14 +10,14 @@ const handler = async (m, { conn, args }) => {
     const apiUrl = `https://eliasar-yt-api.vercel.app/api/ai/text2img?prompt=${prompt}`;
 
     try {
-        conn.reply(m.chat, `🎤💙 ¡Generando tu imagen en el concierto virtual! Por favor espera un momento ✨🎵💫`, m)
+        conn.reply(m.chat, `💙 ¡Generando tu imagen en el concierto virtual! Por favor espera un momento 🎵`, m)
 
         const response = await axios.get(apiUrl, { responseType: 'arraybuffer' });
 
         await conn.sendMessage(m.chat, { image: Buffer.from(response.data) }, { quoted: m });
     } catch (error) {
         console.error('Error al generar la imagen:', error);
-        await conn.reply(m.chat, `🎤💙 ¡Gomen! No se pudo generar la imagen en el mundo virtual. ¡Inténtalo de nuevo más tarde! ✨💫`, m);
+        await conn.reply(m.chat, `💙 ¡Gomen! No se pudo generar la imagen en el mundo virtual. ¡Inténtalo de nuevo más tarde! 💫`, m, rcanal);
     }
 };
 
