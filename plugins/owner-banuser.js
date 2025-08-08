c08a3a2f56c16868af6556b852e5110e3ba98b60
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2025-08-08 15:33:46 -0500

    💙HATSUNE MIKU💙

--- plugins/owner-banuser.js
+++ plugins/owner-banuser.js
@@ -9,7 +9,7 @@ var handler = async (m, { conn, text, usedPrefix, command }) => {
         number = isNaN(text) ? text.split`@`[1] : text
         user = conn.user.jid.split`@`[0] + '@s.whatsapp.net'
         bot = conn.user.jid.split`@`[0]
-        bant = `❀ Por favor, etiqueta o escrive el número del usuario al que quieres banear del Bot.`
+        bant = (m.chat, `💙 Por favor, etiqueta o escrive el número del usuario al que quieres banear del Bot.`, m, rcanal)
         const nn = conn.getName(m.sender)
         if (!text && !m.quoted) return conn.reply(m.chat, bant, m, { mentions: [user] })
         
@@ -22,13 +22,13 @@ var handler = async (m, { conn, text, usedPrefix, command }) => {
         }
 
         number = user.split('@')[0]
-        if (user === conn.user.jid) return conn.reply(m.chat, `✧ @${bot} No puede ser baneado con este comando.`, m, { mentions: [user] })
+        if (user === conn.user.jid) return conn.reply(m.chat, `💙 @${bot} No puede ser baneado con este comando.`, m, rcanal,{ mentions: [user] })
 
         for (let i = 0; i < global.owner.length; i++) {
             ownerNumber = global.owner[i][0]
             if (user.replace(/@s\.whatsapp\.net$/, '') === ownerNumber) {
                 aa = ownerNumber + '@s.whatsapp.net'
-                await conn.reply(m.chat, `✧ No puedo banear al propietario @${ownerNumber} de *${botname}*.`, m, { mentions: [aa] })
+                await conn.reply(m.chat, `💙 No puedo banear al propietario @${ownerNumber} de *${botname}*.`, m, rcanal, { mentions: [aa] })
                 return
             }
         }
@@ -37,13 +37,13 @@ var handler = async (m, { conn, text, usedPrefix, command }) => {
         if (!users[user]) {
             users[user] = { banned: false }
         }
-        if (users[user].banned === true) return conn.reply(m.chat, `✦ No es necesario volver a banear a @${number}.`, m, { mentions: [user] })
+        if (users[user].banned === true) return conn.reply(m.chat, `💙 No es necesario volver a banear a @${number}.`, m, rcanal, { mentions: [user] })
 
         users[user].banned = true
         usr = m.sender.split('@')[0]
-        await conn.reply(m.chat, `❀ Usuario baneado con éxito.`, m, { mentions: [user] })
+        await conn.reply(m.chat, `💙 Usuario baneado con éxito.`, m, rcanal, { mentions: [user] })
         let nametag = conn.getName(user)
-        await conn.reply(`${suittag}@s.whatsapp.net`, `❀ El usuario *${nametag}* ha sido Baneado por *${nn}*.`, m)
+        await conn.reply(`${suittag}@s.whatsapp.net`, `💙 El usuario *${nametag}* ha sido Baneado por *${nn}*.`, m, rcanal)
     } catch (e) {
         await conn.reply(m.chat, `⚠︎ Ocurrió un error.`, m)
     }
