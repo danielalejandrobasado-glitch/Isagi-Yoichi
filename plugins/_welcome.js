95f3996aae3d155170d70c48f5e87dec949282e3
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2025-08-08 19:19:58 -0500

    💙HATSUNE MIKU💙

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -17,11 +17,11 @@ export async function before(m, { conn, participants, groupMetadata }) {
   }
 
   if (chat.welcome && m.messageStubType == 27) {
-    let bienvenida = `🎤✨ *¡Konnichiwa!* Bienvenido al concierto virtual de ${groupMetadata.subject} ✨🎤\n\n💙 @${m.messageStubParameters[0].split`@`[0]} 💙\n\n🎵 ${global.welcom1} 🎵\n\n🌟 ¡Ahora somos ${groupSize} fanáticos de Miku! 🌟\n\n🎶 ¡Prepárate para cantar con nosotros! (◕‿◕)♡ 🎶\n\n> 🎤 Usa *#help* para ver todos los comandos mágicos de Miku! ✨`    
+    let bienvenida = `🎤✨ *¡Konnichiwa!* Bienvenido al concierto virtual de ${groupMetadata.subject} ✨🎤\n\n💙 @${m.messageStubParameters[0].split`@`[0]} 💙\n\n🎵 ${global.welcom1} 🎵\n\n🌟 ¡Ahora somos ${groupSize} fanáticos de Miku! 🌟\n\n🎶 ¡Prepárate para cantar con nosotros! (◕‿◕)♡ 🎶\n\n> 🎤 Usa *#help* para ver todos los comandos mágicos de Miku! ✨\n SIGUE NUESTRO CANAL \n https://www.whatsapp.com/channel/0029VajYamSIHphMAl3ABi1o`    
     await conn.sendMini(m.chat, txt, dev, bienvenida, img, img, redes, fkontak, m, rcanal)
   }
   
   if (chat.welcome && (m.messageStubType == 28 || m.messageStubType == 32)) {
-    let bye = `💫🎵 *¡Sayonara!* ${groupMetadata.subject} te extrañará 🎵💫\n\n💙 @${m.messageStubParameters[0].split`@`[0]} 💙\n\n🎤 ${global.welcom2} 🎤\n\n🌟 Ahora somos ${groupSize} fanáticos esperándote 🌟\n\n🎶 ¡Esperamos verte pronto en nuestro próximo concierto! (｡◕‿◕｡) 🎶\n\n> 🎵 ¡La música de Miku siempre te acompañará! ✨`
+    let bye = `💫🎵 *¡Sayonara!* ${groupMetadata.subject} te extrañará 🎵💫\n\n💙 @${m.messageStubParameters[0].split`@`[0]} 💙\n\n🎤 ${global.welcom2} 🎤\n\n🌟 Ahora somos ${groupSize} fanáticos esperándote 🌟\n\n🎶 ¡Esperamos verte pronto en nuestro próximo concierto! (｡◕‿◕｡) 🎶\n\n> 🎵 ¡La música de Miku siempre te acompañará! ✨\n SIGUE NUESTRO CANAL \n https://www.whatsapp.com/channel/0029VajYamSIHphMAl3ABi1o`
     await conn.sendMini(m.chat, txt1, dev, bye, img, img, redes, fkontak, m, rcanal)
   }}
