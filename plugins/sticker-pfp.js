1e8841575fd0c1e8612b5349859bedac7459c2fb
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2025-08-13 08:24:49 -0500

    💙HATSUNE MIKU💙

--- plugins/sticker-pfp.js
+++ plugins/sticker-pfp.js
@@ -1,12 +1,8 @@
-/* 🎤💙 Código creado por Brauliovh3 
-✧ https://github.com/Brauliovh3/HATSUNE-MIKU.git 
-💙 Hatsune Miku Bot - Virtual Concert Experience 🎵✨ */
-
 let handler = async (m, { conn }) => {
     let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
     let name = conn.getName(who);
-    let pp = await conn.profilePictureUrl(who, 'image').catch(() => 'https://raw.githubusercontent.com/Brauliovh3/HATSUNE-MIKU/main/Contenido/1745522645448.jpeg');
-    await conn.sendFile(m.chat, pp, 'profile.jpg', `*Foto de perfil virtual de ${name}* 🎤💙`, m);
+    let pp = await conn.profilePictureUrl(who, 'image').catch(() => 'https://qu.ax/cAUXY.webp');
+    await conn.sendFile(m.chat, pp, 'profile.jpg', `*Foto de perfil virtual de ${name}* 💙`, m);
 }
 
 handler.help = ['pfp @user'];
