a85216eacc1ae342ec5080b6be30cc5a0b4f6df2
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2025-08-13 08:39:54 -0500

    💙HATSUNE MIKU💙

--- plugins/rpg-trabajar.js
+++ plugins/rpg-trabajar.js
@@ -10,7 +10,7 @@ return
 }
 let rsl = Math.floor(Math.random() * 500)
 cooldowns[m.sender] = Date.now()
-await conn.reply(m.chat, `💙 ${pickRandom(trabajo)} *${toNum(rsl)}* ( *${rsl}* ) ${moneda} 🎵`, m, rcanal)
+await conn.reply(m.chat, ` ${pickRandom(trabajo)} *${toNum(rsl)}* ( *${rsl}* ) ${moneda} `, m, rcanal)
 user.coin += rsl
 }
 
