1df234c6ddf289cfc641758e537a5fe54c00637d
danielalejandrobasado-glitch <danielalejandrobasado@gmail.com>
2025-08-21 18:08:30 -0500

    Update anime-waifu.js

--- plugins/anime-waifu.js
+++ plugins/anime-waifu.js
@@ -3,12 +3,12 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn, usedPrefix, command }) => {
 try {
 await m.react(emojis)
-conn.reply(m.chat, `🎤💙 Buscando una hermosa waifu virtual como yo... ¡Espera un momentito! ✨`, m, rcanal)
+conn.reply(m.chat, `⚽️ Buscando una hermosa waifu ¡Espera un momentito! ⚽️`, m, rcanal)
 let res = await fetch('https://api.waifu.pics/sfw/waifu')
 if (!res.ok) return
 let json = await res.json()
 if (!json.url) return 
-await conn.sendFile(m.chat, json.url, 'thumbnail.jpg', `🎵💙 ¡Aquí tienes tu hermosa waifu virtual! ¿No es kawaii? (◕‿◕)♡ 💙🎵`, m)
+await conn.sendFile(m.chat, json.url, 'thumbnail.jpg', `⚽️ ¡Aquí tienes tu hermosa waifu! ¿No es kawaii? (◕‿◕)♡ ⚽️`, m)
 } catch {
 }}
 handler.help = ['waifu']
